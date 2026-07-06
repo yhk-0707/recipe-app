01541802421611e150959dc2ac2bec7eda01cc26
Yuki-H-23 <h0012.yuri@gmail.com>
2026-07-06 11:38:43 +0900

    update:コメント追加

--- react-learning/src/components/SearchBox.jsx
+++ react-learning/src/components/SearchBox.jsx
@@ -1,9 +1,11 @@
 function SearchBox({ query, onChange }) {
+    // 検索欄のコンポーネント化
+    // value,checkedのpropsで現在のstateを渡す場合は、渡された値を更新するonChangeハンドラも渡す。
     return (
         <input
-            value={query}
-            onChange={e => onChange(e.target.value)}
-            placeholder="材料または料理名"
+            value={query} // 入力欄の値をqueryにバインドする。文字列props
+            onChange={e => onChange(e.target.value)} // 入力欄の値が変更されたときにqueryを更新する。
+            placeholder="材料または料理名" // 入力欄のプレースホルダーを設定する
         />
     );
 }
