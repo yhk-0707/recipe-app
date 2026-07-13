9d615f7108c4e09a5c1cf5a10d2838801f573e7d
Yuki-H-23 <h0012.yuri@gmail.com>
2026-07-13 14:35:29 +0900

    feat:レシピ登録ページへ遷移ボタンを追加

--- nextjs-learning/src/app/page.tsx
+++ nextjs-learning/src/app/page.tsx
@@ -1,9 +1,16 @@
+import Link from 'next/link';
+
 export default function Home() {
   return (
     <main>
       {/* 旧React版の index.html / app.js の searchRecipe() の入口に相当する検索トップ */}
       <h1>料理を検索する</h1>
       <p>料理名や材料名を入力して、登録済みレシピを探します。</p>
+
+      {/* 旧React版の register.html への導線に相当する */}
+      <p>
+        <Link href="/register">レシピ登録へ</Link>
+      </p>
 
       {/* 旧React版では searchButton のクリックや Enter 送信で検索結果ページへ進んでいた */}
       <form action="/recipes" method="GET">
